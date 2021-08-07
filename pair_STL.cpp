05fb901cd0bbf0ce13b4dce90b77c6eea85f9d8f
Hasib <67379993+hasib231@users.noreply.github.com>
2021-08-07 22:21:10 +0600

    Create pair_STL.cpp

---
+++ pair_STL.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    pair <string, int> p;
+    
+    //pair <string, int> p("dipta",21);
+    
+    //p=make_pair("dipta",21);
+    
+    p.first="dipta";
+    p.second=21;
+
+    cout<<p.first<<" "<<p.second<<endl;
+}
