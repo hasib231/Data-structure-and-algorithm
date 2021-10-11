16253cd6fb28e42e06afd348154b618f71807171
Hasib <67379993+hasib231@users.noreply.github.com>
2021-10-11 22:00:51 +0600

    Update graph to matrix.cpp

--- graph to matrix.cpp
+++ graph to matrix.cpp
@@ -34,3 +34,33 @@ int main()
         cout<<endl;
     }
 }
+
+
+//by index of number
+#include<bits/stdc++.h>
+using namespace std;
+
+int adj[100][100];
+
+int main()
+{
+   int node,edge;
+   cout<<"Enter the number of nodes: ";
+   cin>>node;
+   cout<<"Enter the number of edges: ";
+   cin>>edge;
+
+   int n1,n2;
+   for(int i=0;i<edge;i++){
+    cin>>n1>>n2;
+    adj[n1][n2]=1;
+    adj[n2][n1]=1;
+   }
+   for(int i=0;i<node;i++){
+    for(int j=0;j<node;j++){
+        cout<<adj[i][j]<<" ";
+    }
+    cout<<endl;
+   }
+}
+
